81651fe5bd22f41cde5dbfcad44cc7fb9a2dd1fb
Ryan Gasparini <ryan@rxgx.com>
2014-12-13 19:22:19 -0700

    BUGFIX Fixed the redirect page after login

--- login/login.js
+++ login/login.js
@@ -9,7 +9,7 @@ angular.module( 'sample.login', [
       store.set('token', token);
       localStorage.setItem('name', profile.name);
       localStorage.setItem('clientId', profile.user_id);
-      document.location.href= '/game.html';
+      document.location.href= '/Memory-Game/game.html';
     }, function(error) {
       console.log("There was an error logging in", error);
     });
